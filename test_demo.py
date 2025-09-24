ad481da65ef106a0913d92cc91274952d60087f4
ashish8557 <ashishverma.qae@gmail.com>
2025-09-24 06:10:43 +0530

    change

--- test_demo.py
+++ test_demo.py
@@ -6,7 +6,4 @@ import pytest
 def test_add(a,b,c):
     assert c==a+b
 def test_mul_practice():
-    assert 2 * 3 == 7   # intentionally wrong to cause a conflict
-
-def test_mul_practice():
     assert 2 * 3 == 6
