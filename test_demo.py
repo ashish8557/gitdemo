b77d3e841ff7e5fcddf87dee0c416fb161c80586
ashish8557 <ashishverma.qae@gmail.com>
2025-09-24 06:12:07 +0530

    change

--- test_demo.py
+++ test_demo.py
@@ -5,6 +5,5 @@ import pytest
 @pytest.mark.parametrize("a,b,c",[(1,2,3),(2,2,4),(3,3,6),(8,8,16),(9,9,18)])
 def test_add(a,b,c):
     assert c==a+b
-
 def test_mul_practice():
     assert 2 * 3 == 6
