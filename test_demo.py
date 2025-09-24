7d5b5d01d0e7af31dcc03d6e80ca5efdf5edf9fe
ashish8557 <ashishverma.qae@gmail.com>
2025-09-24 06:05:41 +0530

    merge conflict replicate

--- test_demo.py
+++ test_demo.py
@@ -5,4 +5,6 @@ import pytest
 @pytest.mark.parametrize("a,b,c",[(1,2,3),(2,2,4),(3,3,6),(8,8,16),(9,9,18)])
 def test_add(a,b,c):
     assert c==a+b
+def test_mul_practice():
+    assert 2 * 3 == 7   # intentionally wrong to cause a conflict
 
